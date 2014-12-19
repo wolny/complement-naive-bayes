c09562e227d00784473171b1f8b28ba850d4664b
Adrian Wolny <adrian.wolny@dawanda.com>
2014-12-19 15:34:04 +0100

    example how to use the API

---
+++ src/main/java/com/dawanda/ApiUsageExample.java
@@ -0,0 +1,45 @@
+package com.dawanda;
+
+import com.dawanda.classifier.DocumentClassifier;
+import com.dawanda.classifier.LabelingResult;
+import com.dawanda.classifier.naivebayes.NaiveBayesModel;
+import com.dawanda.classifier.naivebayes.WeightNormalizedComplementNaiveBayes;
+import com.dawanda.document.Document;
+import com.dawanda.utils.Extractors;
+import com.dawanda.utils.NaiveBayesSerializer;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by awolny on 19/12/14.
+ */
+public class ApiUsageExample {
+    public static void main(String[] args) throws IOException {
+        // read Naive Bayes model from JSON file
+        String pathToModel = "./model.json";
+        NaiveBayesModel model = NaiveBayesSerializer.readFrom(pathToModel);
+
+        // create Complement Naive Bayes classifier
+        DocumentClassifier classifier = new WeightNormalizedComplementNaiveBayes(model);
+
+        // get the title and description of the product which is to be labeled
+        String title = "...";
+        String description = "...";
+        String text = title + " " + description;
+
+        // extract features, MAKE SURE THE SAME EXTRACTOR WAS USED DURING TRAINING PHASE
+        Document document = Extractors.STANDARD_EXTRACTOR.extractFeatureVector(text);
+
+        // label document
+        LabelingResult labelingResult = classifier.label(document);
+
+        // get categories ordered by score
+        List<LabelingResult.ScoredCategory> categories = labelingResult.getOrderedCategories();
+
+        // print 3 best category suggestions according to the model
+        System.out.println(Lists.newArrayList(Iterables.limit(categories, 3)));
+    }
+}
